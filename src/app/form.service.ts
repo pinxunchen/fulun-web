fccb4d56c73e8ca329de485cc930a7d4947ecc99
Pin <apy9510@yahoo.com.tw>
2023-11-07 15:48:41 +0800

    更新訂車日期選擇器

--- src/app/form.service.ts
+++ src/app/form.service.ts
@@ -2,41 +2,42 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FormService {
   private apiUrl = 'https://www.crowntaxi.com.tw/CTW/Service.svc/';
 
-  DUID:string = "";
+  DUID: string = '';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   //表單提交
   submitForm(formData: FormData): Observable<any> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' ,'Accept': 'application/json; charset=utf-8'});
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Accept: 'application/json; charset=utf-8',
+    });
     const httpOptions = { headers: headers };
-    return this.http.post<any>(this.apiUrl + 'LTCAppoint', formData, httpOptions);
+    return this.http.post<any>(
+      this.apiUrl + 'LTCAppointV2',
+      formData,
+      httpOptions
+    );
   }
-
 
   //提交查詢交通趟紀錄
   sendData(data: any): Observable<any> {
     return this.http.post<any>(this.apiUrl + 'QueryLTCAppoint', data);
   }
 
-
   //查詢長照額度
   searchQuotaService(data: any): Observable<any> {
     return this.http.post<any>(this.apiUrl + 'QueryLTCQuota', data);
   }
 
-
   //查詢新北長照資格
   searchQualificationsService(data: any): Observable<any> {
     return this.http.post<any>(this.apiUrl + 'QueryLTCCase', data);
   }
-
-
 }
--- src/app/page/menu/booking/taipei/taipei.component.ts
+++ src/app/page/menu/booking/taipei/taipei.component.ts
@@ -1,131 +1,201 @@
-import { Component ,OnInit} from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { FormService } from './../../../../form.service';
-import { FormBuilder  , Validators } from '@angular/forms';
-import { ActivatedRoute , Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
-
+import { PrimeNGConfig } from 'primeng/api';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-taipei',
   templateUrl: './taipei.component.html',
-  styleUrls: ['./taipei.component.css']
+  styleUrls: ['./taipei.component.css'],
 })
-
 export class TaipeiComponent implements OnInit {
   passengerName: string | null = null;
   passengerId: string | null = null;
   puAddress: string | null = null;
   dpAddress: string | null = null;
   telephone: string | null = null;
-  DUID: string = "";
-
+  DUID: string = '';
   form: any;
-
   buttonText: string = '提交';
   isSubmitting: boolean = false;
+  isLoading: boolean = false;
+  ch: any;
 
-  constructor(private fb: FormBuilder ,
+  constructor(
+    private fb: FormBuilder,
     private formService: FormService,
     private route: ActivatedRoute,
     private router: Router,
-    private datePipe: DatePipe)
-     {
-      const defaultTime = new Date();
-      defaultTime.setHours(8, 0, 0, 0);
+    private datePipe: DatePipe,
+    private primengConfig: PrimeNGConfig
+  ) {
+    this.primengConfig.setTranslation({
+      firstDayOfWeek: 0,
+      dayNames: ['日', '一', '二', '三', '四', '五', '六'],
+      dayNamesShort: ['日', '一', '二', '三', '四', '五', '六'],
+      dayNamesMin: ['日', '一', '二', '三', '四', '五', '六'],
+      monthNames: [
+        '一月',
+        '二月',
+        '三月',
+        '四月',
+        '五月',
+        '六月',
+        '七月',
+        '八月',
+        '九月',
+        '十月',
+        '十一月',
+        '十二月',
+      ],
+      monthNamesShort: [
+        '一月',
+        '二月',
+        '三月',
+        '四月',
+        '五月',
+        '六月',
+        '七月',
+        '八月',
+        '九月',
+        '十月',
+        '十一月',
+        '十二月',
+      ],
+      today: '今天',
+      clear: '清除',
+      dateFormat: 'yy-mm-dd',
+      weekHeader: 'Wk',
+    });
 
-     this.form = this.fb.group({
-     PassengerName: ['', [Validators.required]],
-     PassengerId: ['', [Validators.required, Validators.minLength(10)]],
-     Date: ['', [Validators.required]],
-     RTime: [defaultTime, [Validators.required]],
-     BTime: [defaultTime],
-     Area: '台北',
-     DUID: [this.formService.DUID, [Validators.required]],
-     PUAddress: ['', [Validators.required]],
-     DPAddress: ['', [Validators.required]],
-     Telephone: ['', [Validators.required, Validators.minLength(10)]],
-     Remark: '',
-     Option: ['爬梯機+交通', [Validators.required]],
-   });
- }
+    this.form = this.fb.group({
+      PassengerName: ['', [Validators.required]],
+      PassengerId: ['', [Validators.required, Validators.minLength(10)]],
+      Dates: ['', [Validators.required]],
+      RTime: ['', [Validators.required]],
+      BTime: [''],
+      Area: '台北',
+      DUID: [this.formService.DUID, [Validators.required]],
+      PUAddress: ['', [Validators.required]],
+      DPAddress: ['', [Validators.required]],
+      Telephone: ['', [Validators.required, Validators.minLength(10)]],
+      Remark: '',
+      Option: ['爬梯機+交通', [Validators.required]],
+    });
+  }
 
-
-
- onSubmit(f:any){
-   if (f.valid) {
-     //const duidValue = this.form.get('DUID').value;
-     this.isSubmitting = true;
-     const formData = { ...this.form.value, DUID: this.formService.DUID };
-     formData.RTime = this.formatTime(formData.RTime as Date);
-     formData.BTime = this.formatTime(formData.BTime as Date);
-     this.formService.submitForm(formData)
-       .subscribe(
-         response => {
-           //console.log('請求成功：', formData);
-           this.buttonText = '提交中...';
-           const message = `<b>個案姓名</b> : ${formData.PassengerName}
-           <br><b>日期</b> : ${formData.Date}
+  onSubmit(f: any) {
+    this.isLoading = true;
+    if (f.valid) {
+      this.isSubmitting = true;
+      const formData = { ...this.form.value, DUID: this.formService.DUID };
+      formData.RTime = this.formatTime(formData.RTime as Date);
+      formData.BTime = this.formatTime(formData.BTime as Date);
+      formData.Dates = this.formatDates(formData.Dates as Date[]);
+      this.formService.submitForm(formData).subscribe(
+        (response) => {
+          //console.log('請求成功：', formData);
+          this.buttonText = '提交中...';
+          const message = `<b>個案姓名</b> : ${formData.PassengerName}
+           <br><b>日期</b> : ${formData.Dates}
            <br><b>去程時間</b> : ${formData.RTime}
            <br><b>回程時間</b> : ${formData.BTime}
            <br><b>出發地點</b> : ${formData.PUAddress}
            <br><b>目的地點</b> : ${formData.DPAddress}`;
-           this.showAlert(message);
-           this.form.reset();
-           // 跳轉回menu
-           this.router.navigate(['/menu']);
-         },
-         error => {
-           //console.error('請求錯誤：', error);
-           this.buttonText = '提交';
-           this.isSubmitting = false;
-         }
-       );
-   } else {
-     const message = '請重新確認表單內容！'
-     this.showFailAlert(message);
-     const duidValue = this.form.get('DUID').value;
-     this.buttonText = '提交';
-     this.isSubmitting = false;
-    //console.log('DUID:', duidValue);
-   }
- }
-
- ngOnInit() {
-   this.route.queryParams.subscribe(params => {
-     this.passengerName = params['passengerName'] || null;
-     this.passengerId = params['passengerId'] || null;
-     this.puAddress = params['puAddress'] || null;
-     this.dpAddress = params['dpAddress'] || null;
-     this.telephone = params['telephone'] || null;
-   });
- }
-
- showAlert(message: string): void {
-   Swal.fire({
-     title: `<b>提交成功！</b>`,
-     html: `<b>${message}</b>`,
-     icon: 'success',
-     confirmButtonText: '確定'
-   });
- }
-
- showFailAlert(message: string): void {
-   Swal.fire({
-     html: `<b>${message}</b>`,
-     icon: 'error',
-     confirmButtonText: '確定'
-   });
- }
-
- formatTime(date: Date | null): string {
-  if (date) {
-    return this.datePipe.transform(date, 'HH:mm') || '';
-  } else {
-    return '無回程';
+          this.showAlert(message);
+          this.form.reset();
+          this.router.navigate(['/menu']);
+          this.isLoading = false;
+        },
+        (error) => {
+          //console.error('請求錯誤：', formData);
+          this.buttonText = '提交';
+          this.isSubmitting = false;
+          this.isLoading = false;
+        }
+      );
+    } else {
+      const message = '請重新確認表單內容！';
+      this.showFailAlert(message);
+      this.buttonText = '提交';
+      this.isSubmitting = false;
+      this.isLoading = false;
+    }
   }
-}
 
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.passengerName = params['passengerName'] || null;
+      this.passengerId = params['passengerId'] || null;
+      this.puAddress = params['puAddress'] || null;
+      this.dpAddress = params['dpAddress'] || null;
+      this.telephone = params['telephone'] || null;
+    });
+  }
 
+  showAlert(message: string): void {
+    Swal.fire({
+      title: `<b>提交成功！</b>`,
+      html: `<b>${message}</b>`,
+      icon: 'success',
+      confirmButtonText: '確定',
+    });
+  }
+
+  showFailAlert(message: string): void {
+    Swal.fire({
+      html: `<b>${message}</b>`,
+      icon: 'error',
+      confirmButtonText: '確定',
+    });
+  }
+
+  //檢查回程時間是否有填寫
+  formatTime(date: Date | null): string {
+    if (date) {
+      return this.datePipe.transform(date, 'HH:mm') || '';
+    } else {
+      return '無回程';
+    }
+  }
+
+  //轉換日期格式
+  formatDate(date: string | null): string {
+    if (date) {
+      return this.datePipe.transform(date, 'yyyy-MM-dd') || '';
+    } else {
+      return '';
+    }
+  }
+
+  //設定回程初始時間
+  setInitialTime() {
+    const defaultTime = new Date();
+    defaultTime.setHours(13, 0, 0, 0);
+    this.form.get('BTime').setValue(defaultTime);
+  }
+
+  //設定去程初始時間
+  setInitialTime2() {
+    const defaultTime = new Date();
+    defaultTime.setHours(10, 0, 0, 0);
+    this.form.get('RTime').setValue(defaultTime);
+  }
+
+  //檢查是否有多天日期
+  formatDates(dates: Date[] | null): string {
+    if (dates && dates.length > 0) {
+      const formattedDates = dates.map((date) =>
+        this.datePipe.transform(date, 'yyyy-MM-dd')
+      );
+      return dates
+        .map((date) => this.datePipe.transform(date, 'yyyy-MM-dd') || '')
+        .join(', ');
+    } else {
+      return '';
+    }
+  }
 }
